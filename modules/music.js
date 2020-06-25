ae103bfef174d6efcccd14da916cafc865332baf
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-06-24 20:51:19 -0700

    music rendering issues

--- modules/music.js
+++ modules/music.js
@@ -17,31 +17,21 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', err => console.log(err));
 
 
-
-app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-app.use('/public', express.static('public'));
-
-const client = new pg.Client(process.env.DATABASE_URL);
-client.on('error', err => console.log(err));
-
 const getTunes = (request, response) => {
   console.log('In the music function', request.query);
-  let city = 'kalispell';
-  let tunesUrl = `http://musicovery.com/api/V6/artist.php?fct=getfromlocation&location=${city}`;
-
+  // let city = 'kalispell';
+  let tunesUrl = `http://musicovery.com/api/V6/track.php?=new&track=false`
   superagent.get(tunesUrl)
     .then(request => {
       // console.log(request.body.artists.artist)
       let tunesArray = request.body.artists.artist.map(oneTune => {
         return new Song(oneTune);
       });
-      console.log('Tunes Array', tunesArray);
+      // console.log('Tunes Array', tunesArray);
 
 
       response.status(200).render('resultsFromMusic.ejs',
-
-        {tunesResults: tunesArray});
+    {tunesResults: tunesArray});
     }).catch(error => {
       console.error('error', error)
     })
